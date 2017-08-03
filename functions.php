e627806d8bc33ca28a1207fdd025505a47f5e2e2
hykdje <heykadyje@gmail.com>
2017-08-03 14:11:09 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -15,4 +15,19 @@ function theme_js() {
 
 }
 
+<!— functions.php file —>
+add_theme_support( ‘menus’ );
+
+function register_theme_menus() {
+	register_nav_menus(
+		array(
+			‘header-menu’  => __( ‘Header Menu’ );
+		)
+	);
+
+}
+
+add_action( ‘init’, ‘register_theme_menus’ );
+
+
 ?>
