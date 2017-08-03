b053ed83d48a37509c2e8520bfba45bbc7a35426
hykdje <heykadyje@gmail.com>
2017-08-03 13:50:22 -0400

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<html>
+<head>
+
+<title>Jumbotron Template for Bootstrap</title>
+
+<!—Bootstrap core CSS—>
+<link href=“../..dis/css/bootstrap.min.css” rel=“stylesheet”>
+
+<!—Custom styles for this template
+<link href=“jumbotron.css” rel=“stylesheet”>
+—>
+
+</head>
+</html>
+
+<?php get_footer(); ?>
